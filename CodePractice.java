846145ed927ea86fde65ea758285098706fac9cb
ritik <ritikmishra836@gmail.com>
2025-02-16 17:48:14 +0530

    learn branch

--- CodePractice.java
+++ CodePractice.java
@@ -3,5 +3,6 @@ package dsakunalkushwaha;
 public class CodePractice {
     public static void main(String[] args) {
         System.out.println("I will practice coding in here");
+        System.out.println("learn branch in code");
     }
 }
